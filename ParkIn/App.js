4e536cefa8a8d1c57fd0c2fa139d6c9ce37ef525
Prasenjit <anitadarole79@gmail.com>
2022-06-06 12:47:39 +0530

    first commit

--- ParkIn/App.js
+++ ParkIn/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello 12222!</Text>
+      <Text>Hello 2!</Text>
       <StatusBar style="auto" />
     </View>
   );
